ae15ff9cb6c94271b1477312ab3fdd1a6544894a
Nicole Durham <nicole.martinez.durham@gmail.com>
2024-01-21 15:24:19 -0500

    Resize and convert photo, choose from gallery option, update Netinfo

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -6,6 +6,8 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import * as ImagePicker from "expo-image-picker";
 import { baseUrl } from "../shared/baseUrl";
 import logo from "../assets/images/logo.png";
+import * as ImageManipulator from "expo-image-manipulator";
+import * as MediaLibrary from "expo-media-library";
 
 const LoginTab = ({ navigation }) => {
   const [username, setUsername] = useState("");
@@ -144,9 +146,38 @@ const RegisterTab = () => {
         allowsEditing: true,
         aspect: [1, 1],
       });
+
       if (capturedImage.assets) {
         console.log(capturedImage.assets[0]);
-        setImageUrl(capturedImage.assets[0].uri);
+        processImage(capturedImage.assets[0].uri);
+      }
+    }
+  };
+
+  const processImage = async (imgUri) => {
+    const processedImage = await ImageManipulator.manipulateAsync(
+      imgUri,
+      [{ resize: { width: 400 } }],
+      { format: "png" }
+    );
+
+    console.log(processedImage);
+    setImageUrl(processedImage.uri);
+    MediaLibrary.saveToLibraryAsync(processedImage.uri);
+  };
+
+  const getImagefromGallery = async () => {
+    const mediaLibraryPermissions =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (mediaLibraryPermissions.status === "granted") {
+      const capturedImage = await ImagePicker.launchImageLibraryAsync({
+        allowsEditing: true,
+        aspect: [1, 1],
+      });
+
+      if (capturedImage.assets) {
+        console.log(capturedImage.assets[0]);
+        processImage(capturedImage.assets[0].uri);
       }
     }
   };
@@ -161,6 +192,7 @@ const RegisterTab = () => {
             style={styles.image}
           />
           <Button title="Camera" onPress={getImagefromCamera} />
+          <Button title="Gallery" onPress={getImagefromGallery} />
         </View>
         <Input
           placeholder="Username"
